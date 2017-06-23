b9abb5db3fd0025a25c36ac46349957211e6d522
Barbara <Barbara@10.207.6.96>
2017-06-23 10:39:12 +0200

    Test

--- Ladybird/src/aktion/Test.java
+++ Ladybird/src/aktion/Test.java
@@ -1,5 +1,26 @@
 package aktion;
 
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.Keys;
+import lejos.hardware.lcd.TextLCD;
+
+
 public class Test {
 
+	public static void main(String[] args) {
+		//EV3-Stein suchen&finden
+		EV3 ev3brick = (EV3) BrickFinder.getLocal();
+		
+		//Buttons und LCD-Display instantiieren
+		Keys buttons = ev3brick.getKeys();
+		TextLCD lcddisplay = ev3brick.getTextLCD();
+		
+		//Test auf LCD-Display schreiben
+		lcddisplay.drawString("Helloworld",2,4);
+		
+		//auf Reaktion des Anwenders der Testinstallation warten
+		buttons.waitForAnyPress();
+		
+	}
 }
